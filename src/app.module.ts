333843f2327710f01f0c74c09ec55776111fb1e4
franklinramon <140756834+franklinramon@users.noreply.github.com>
2024-12-09 15:47:17 -0600

    creacion

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT, 10) || 5432,
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASS || 'password',
+      database: process.env.DB_NAME || 'nestjsdb',
+      autoLoadEntities: true, // Automatically load entities
+      synchronize: true, // Set to false in production
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
